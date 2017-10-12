66cf69cea2f976ed9edc0cfab76e73349ee2f39d
kaistullich <kairstullich@gmail.com>
2017-10-12 10:58:03 -0700

    regex for checking weather and in, and use weather class

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,8 @@ import re
 from flask import Flask, render_template, jsonify, request
 from rivescript import RiveScript
 
+from get_weather import Weather
+
 app = Flask(__name__)
 
 # initialize RiveScript stuff
@@ -19,12 +21,20 @@ def index():
 
 @app.route('/reply', methods=['POST'])
 def reply():
-    msg = request.json['userMsg']
-    regex = re.compile(r'weather')
+    # capture what the user said
+    user_msg = request.json['userMsg']
+    # check if "weather" and "in" are in inside the user input
+    weather_regex = re.compile(r'^(?=.*weather)(?=.*in)')
 
     # TODO: continue integrating weather stuff
-    if regex.search(msg):
-        print()
+    if weather_regex.search(user_msg):
+        print('weather and in, inside of string')
+        # get all the characters after "in" and remove whitespace
+        get_city = user_msg[user_msg.find('in') + 2:].strip()
+        w = Weather()
+        w.get_woeid(get_city)
+        weather_data = w.get_weather()
+        return jsonify({"weather_data": weather_data})
     else:
-        botreply = bot.reply('localuser', msg)
+        botreply = bot.reply('localuser', user_msg)
         return jsonify({"reply": botreply})
