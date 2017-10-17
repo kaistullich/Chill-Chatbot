d86a385b843c03225ce18d7c6e3f0e0d6f25e304
kaistullich <kairstullich@gmail.com>
2017-10-17 12:45:32 -0700

    edited JS file for weathericons, and loop through 5 day in JS

--- static/js/send_reply.js
+++ static/js/send_reply.js
@@ -10,16 +10,11 @@ btn.click(() => {
         type: 'POST',
         data: JSON.stringify(msg, null, 2),
         contentType: 'application/json;charset=UTF-8',
-        success: (resp) => {
-            if (resp.weather_data) {
-                // console.log(resp);
-                // grab current temperature
-                let curTemp = resp.weather_data.query.results.channel.item['condition'].temp;
-                // grab condition description (i.e. sunny, cloudy)
-                let cond = resp.weather_data.query.results.channel.item['condition']['text'];
-                bot.text(resp)
+        success: (recvWeatherData) => {
+            if (recvWeatherData.weather_data) {
+                fiveDayWeather(recvWeatherData)
             } else {
-                bot.text(resp.reply);
+                bot.text(recvWeatherData.reply);
             }
         },
         error: (err) => {
@@ -27,3 +22,23 @@ btn.click(() => {
         }
     })
 });
+
+
+function fiveDayWeather(weather) {
+    // 5 day forecast
+    let fiveDay = weather.weather_data['query']['results']['channel']['item']['forecast'];
+    // loop through array
+    for (let i = 0; i < fiveDay.length; i++) {
+        console.log(`High today: ${fiveDay[i].high}`)
+    }
+    // grab current temperature
+    let curTemp = weather.weather_data.query.results.channel.item['condition'].temp;
+    // grab condition description (i.e. sunny, cloudy)
+    let cond = weather.weather_data.query.results.channel.item['condition']['text'];
+
+    WeatherIcon.add('day1', WeatherIcon.SUN, {mode:WeatherIcon.DAY, stroke:true, shadow:true, animated:true});
+    WeatherIcon.add('day2', WeatherIcon.LIGHTRAINSUN, {mode:WeatherIcon.NIGHT, stroke:true, shadow:true, animated:true});
+    WeatherIcon.add('day3', WeatherIcon.LIGHTRAINTHUNDERSUN); // no parameters
+
+    bot.text(weather);
+}
